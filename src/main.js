655bdba5ab175752cd26f16f216d1f0f4d6f5413
metuta <34999683+metuta@users.noreply.github.com>
2018-01-02 11:35:24 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,61 +1,66 @@
-function getData(url){
-  return new Promise(function(resolve, reject){
+function getData(url) {
+  return new Promise(function(resolve, reject) {
     var req = new XMLHttpRequest();
     req.open('GET', url);
-    req.onload = () => (req.status == 200 && req.status < 300) ? resolve(req.response) : reject({status: req.status, statusText: req.statusText});    
-    req.onerror = () => reject({status: req.status, statusText: req.statusText});
+    req.onload = () => (req.status == 200 && req.status < 300) ? resolve(req.response) : reject({
+      status: req.status,
+      statusText: req.statusText
+    });
+    req.onerror = () => reject({
+      status: req.status,
+      statusText: req.statusText
+    });
     req.send();
   });
 }
 
 function loadProducts() {
-  getData('http://api.cewe-community.com/v2/products').then(function(res){	  
-	  let products = JSON.parse(res);
-	  let tmpl = products.data.products.length + ' Produkte wurden geladen!';	  
-	  saveToLS(JSON.stringify(products.data.products));
-	  document.getElementById('template').innerHTML = tmpl;
-	}).catch(function(err){
-	  let tmpl = `
+  getData('http://api.cewe-community.com/v2/products').then(function(res) {
+    let products = JSON.parse(res);
+    let tmpl = products.data.products.length + ' Produkte wurden geladen!';
+    saveToLS(JSON.stringify(products.data.products));
+    document.getElementById('template').innerHTML = tmpl;
+  }).catch(function(err) {
+    let tmpl = `
 	  	Fehler: ${err.statusText}!
 	  `;
-	  document.getElementById('template').innerHTML = tmpl;
-	});
+    document.getElementById('template').innerHTML = tmpl;
+  });
 }
 
 function saveToLS(products) {
-	localStorage.setItem('products', products);
+  localStorage.setItem('products', products);
 }
 
 function findProduct() {
-	let p_id = document.getElementById('p_id').value;
+  let p_id = document.getElementById('p_id').value;
 
-	if(p_id == "") {
-		let tmpl = `
+  if (p_id == "") {
+    let tmpl = `
 	  	Fehler: Bitte geben Sie ein Produkt-ID!
 	  `;
-	  document.getElementById('template').innerHTML = tmpl;
-		return false;
-	}
+    document.getElementById('template').innerHTML = tmpl;
+    return false;
+  }
 
-	let products = JSON.parse(localStorage.getItem("products"));	
+  let products = JSON.parse(localStorage.getItem("products"));
 
-	let tmpl = ``;
+  let tmpl = ``;
 
   let product = products.find(p => p.ID === p_id);
-  if(product) {
-	  tmpl += `
+  if (product) {
+    tmpl += `
 	    Ergebnis für Produkt: ${p_id}
 	    <br />
 	    <h3>${product.Title}</h3>
 	    <p>Erstellt am ${product.Created} von ${product.UserName} (UserID: ${product.UserID})</p>
 	    <p><img src=${product.CoverURL} /></p>	     
 	  `;
-	}
-	else {
-		tmpl += `
+  } else {
+    tmpl += `
 	  	Fehler: Produkt ${p_id} konnte nicht gefunden werden!
 	  `;
-	}
+  }
 
   document.getElementById('template').innerHTML = tmpl;
   return true;
